33d7665731e5b57af115a3e787a9123c5c4170c6
zhongderer <1039024361@qq.com>
2020-05-12 07:40:23 +0800

    curry

---
+++ 20200512 curry.js
@@ -0,0 +1,7 @@
+// simple curry
+function sun_curry (fn) {
+    var args = [].slice.call(arguments, 1)
+    return function () {
+        return fn.apply(this, args.concat([].slice.call(arguments)))
+    }
+}
