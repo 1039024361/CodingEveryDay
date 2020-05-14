60e4604452c1db2884ac32b152e830c9c997e456
zhongderer <1039024361@qq.com>
2020-05-14 18:16:38 +0800

    update

--- 20200512 curry.js
+++ 20200512 curry.js
@@ -1,7 +1,21 @@
 // simple curry
-function sun_curry (fn) {
+function sub_curry (fn) {
     var args = [].slice.call(arguments, 1)
     return function () {
         return fn.apply(this, args.concat([].slice.call(arguments)))
+    }
+}
+
+// generate curry
+function curry (fn, length) {
+    length = length || fn.length
+    var slice = Array.prototype.slice
+    return function () {
+        var args = slice.call(arguments)
+        if (args.length >= length) {
+            return fn.apply(this, args)
+        } else {
+            return curry(sub_curry.apply([fn].concat(args)), length - args.length)
+        }
     }
 }
