9ac5f727a57e9be71002ccc478031f92c376a718
zhongderer <1039024361@qq.com>
2020-05-22 18:11:33 +0800

    update quick sort

--- 20200517 quickSort.js
+++ 20200517 quickSort.js
@@ -1,19 +1,27 @@
-var quicSort = function sort (arr, i, j)  {
-    if (i <= j) return
-    var start = i
-    var end = j
-    var ref = arr[end]
-    while (start < end) {
-        while (start < end && arr[start] < ref) {
-            start++
+var quicSort = function (arr) {
+    var sort = function (arr, i = 0, j = arr.length -1)  {
+        if (i >= j) return
+        var start = i
+        var end = j
+        var ref = arr[end]
+        while (start < end) {
+            while (start < end && arr[start] <= ref) {
+                start++
+            }
+            arr[end] = arr[start]
+            while (start < end && arr[end] >= ref) {
+                end--
+            }
+            arr[start] = arr[end]
         }
-        arr[end--] = arr[start]
-        while (start < end && arr[end] > ref) {
-            end--
-        }
-        arr[start++] = arr[end]
+        arr[start] = ref
+        sort(arr, i, start - 1)
+        sort(arr, start + 1, j)
     }
-    arr[start] = ref
-    sort(arr, i, start)
-    sort(arr, start + 1, j)
+    var newArr = arr.concat()
+    sort(newArr)
+    return newArr
 }
+
+var arr = [1, 4, 67, 12, 45, 123]
+var arr1 = [1, 4, 67, 0, 5, 5, 6, 3453, 12, 45, 123]
